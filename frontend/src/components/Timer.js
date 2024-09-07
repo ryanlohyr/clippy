988a17629c32e41ab46f7b34ad551712e948c99f
shayaansultan <sultan.shayaan@gmail.com>
2024-09-07 15:59:10 -0700

    update timer

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -3,6 +3,22 @@
 import React, { useState, useEffect } from "react";
 
 const Timer = () => {
+  // Get the params from the URL
+  const params = new URLSearchParams(window.location.search);
+  const is_reset = params.get("is_reset");
+
+  if (is_reset === "true") {
+    return (
+      <div className="absolute top-0 w-full bg-black text-white p-4 flex justify-center">
+        <h1 className="text-xl font-bold">
+          {
+            "I told u not too leave!! Now ur stuck here with me for a random time :)"
+          }
+        </h1>
+      </div>
+    );
+  }
+
   const [timeLeft, setTimeLeft] = useState(20); // Initialize the timer at 30 seconds
 
 
