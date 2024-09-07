e1f8b9346235b585c95bcd4d2c3ac96761add2e4
Ryan Loh <ryanloh29@gmail.com>
2024-09-07 16:03:14 -0700

    Fix Lint

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -3,6 +3,16 @@
 import React, { useState, useEffect } from "react";
 
 const Timer = () => {
+  const [timeLeft, setTimeLeft] = useState(20); // Initialize the timer at 30 seconds
+
+  useEffect(() => {
+    // Create a countdown effect
+    if (timeLeft > 0) {
+      const timerId = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearTimeout(timerId); // Cleanup the timer
+    }
+  }, [timeLeft]);
+
   // Get the params from the URL
   const params = new URLSearchParams(window.location.search);
   const is_reset = params.get("is_reset");
@@ -19,22 +29,12 @@ const Timer = () => {
     );
   }
 
-  const [timeLeft, setTimeLeft] = useState(20); // Initialize the timer at 30 seconds
-
-
-
-  useEffect(() => {
-    // Create a countdown effect
-    if (timeLeft > 0) {
-      const timerId = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
-      return () => clearTimeout(timerId); // Cleanup the timer
-    }
-  }, [timeLeft]);
-
   return (
     <div className="absolute top-0 w-full bg-black text-white p-4 flex justify-center">
       {timeLeft === 0 ? (
-        <h1 className="text-xl font-bold">I want you to stay here and be useless</h1>
+        <h1 className="text-xl font-bold">
+          I want you to stay here and be useless
+        </h1>
       ) : (
         <h1 className="text-xl font-bold">
           You need to stay on this site for {timeLeft} seconds xd xd xd
